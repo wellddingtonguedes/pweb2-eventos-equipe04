61f1053e8c748e3218a385c32c1ddf59d03de284
Gabriel Lins <123127213+GlinsC@users.noreply.github.com>
2026-03-05 20:49:11 -0300

    Add PUT endpoint for updating eventos (Ainda Tem problema)

--- server.js
+++ server.js
@@ -20,6 +20,12 @@ app.post('/eventos', (req, res) => {
 })
 
 
-
+app.put('/eventos/:id', (req, res) => {
+    console.log(req.params)
+    const {id} = req.params
+    
+    const dadosModificado = db.atualizar(id, req.body)
+    res.status(200).json(dadosModificado)
+})
 
 app.listen(3000)
